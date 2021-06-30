4c423dc1c65fbecea657bbdea16b44720697dd9b
Ileis <gabrielileis07@gmail.com>
2021-06-30 16:51:35 -0300

    ContaBancaria

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -4,40 +4,85 @@
  */
 public class Conta {
 
+    private final double LIM_MAX = 100;
+    private final int conta;
+    private double limite = LIM_MAX;
+    private double saldo;
+    private double[] extrato = new double[10];
+    private int operacao;
+
+
     public Conta(int numero, double saldo) {
+        this.conta = numero;
+        this.saldo = saldo;
     }
 
     
     public int getNumero() {
-        return 0;
+        return conta;
     }
     
     public double getSaldo() {
-        return 0;
+        return (saldo + limite);
     }
-
     
     public double getLimite() {
-        return 0;
+        return limite;
+    }
+
+    private void controleSaldoLimite(double valor){
+
+        if(valor > 0){
+            limite += valor;
+
+            if(limite > LIM_MAX){
+                saldo += (limite - LIM_MAX);
+                limite = LIM_MAX;
+            }
+        }else{
+            saldo += valor;
+
+            if(saldo < 0){
+                limite += saldo;
+                saldo = 0;
+            }
+        }
+
+        extrato[operacao] = valor;
+        operacao++;
     }
     
     public boolean sacar(double valor) {
+        if((valor > 0) && (valor <= getSaldo())){
+            controleSaldoLimite(-valor);
+
+            return true;
+        }
         return false;
     }
 
    
     public boolean depositar(double valor) {
-        return true;
+        if(valor > 0){
+            controleSaldoLimite(valor);
+
+            return true;
+        }
+        return false;
     }
 
     
     public boolean transferir(Conta destino, double valor) {
+        if((valor > 0) && (valor <= getSaldo())){
+            controleSaldoLimite(-valor);
+            destino.depositar(valor);
+
+            return true;
+        }
         return false;
     }
 
-
     public double[] verExtrato() {
-
-        return null;
+        return extrato;
     }
 }
